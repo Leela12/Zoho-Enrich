ad13e759d4a072c466af6efffd0a1d0a3798a7ae
Leela12 <36850884+Leela12@users.noreply.github.com>
2018-03-01 17:20:33 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+
+int main() 
+{
+   
+	int x,n,i,a[30];
+
+	printf("Number to find:");
+  	scanf("%d",&x);
+   
+	scanf("%d",&n);
+ 
+	printf("Array:");  
+	for(i=0;i<n;i++)
+   
+	{
+       
+		scanf("%d ",&a[i]);
+   
+	}
+   
+	for(i=0;i<n;i++)
+   
+	{
+       
+		if(a[i]==x)
+       
+		{
+           
+			printf("%d",i);
+           
+			break;
+       
+		}
+       
+		else if(i==n-1&&a[i]!=x)
+       
+		{
+           
+			printf("-1");
+       
+		}
+   
+	}
+
+}
