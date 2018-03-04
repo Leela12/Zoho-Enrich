aaf5ec05c9bb156bf95573373769708f853198fe
Leela12 <36850884+Leela12@users.noreply.github.com>
2018-03-04 17:18:55 +0530

    Add files via upload

---
+++ voweloccur.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+	char s[30],a[]="aeiou";
+	int c[5]={0},i,l;
+	scanf("%[^\n]",s);
+	l=strlen(s);
+	for(i=0;i<l;i++)
+	{
+		if(s[i]=='A'||s[i]=='a')
+		{
+			c[0]++;
+		}
+		else if(s[i]=='E'||s[i]=='e')
+		{
+			c[1]++;
+		}
+		
+		else if(s[i]=='I'||s[i]=='i')
+		{
+			c[2]++;
+		}
+		
+		else if(s[i]=='O'||s[i]=='o')
+		{
+			c[3]++;
+		}
+		
+		else if(s[i]=='U'||s[i]=='u')
+		{
+			c[4]++;
+		}
+	}
+	for(i=0;i<l;i++)
+	{
+		if(c[i]!=0)
+		{
+			printf("%c %d\n",a[i],c[i]);
+		}
+	}
+}
+		
