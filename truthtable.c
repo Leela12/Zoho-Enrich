df6ad8c34a9cde5e8a547a03731cb76481f73774
Leela12 <36850884+Leela12@users.noreply.github.com>
2018-03-29 22:50:50 +0530

    Add files via upload

---
+++ truthtable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+        printf("A\tB\tC\tY\n");
+	for(a=0;a<=1;a++)
+	{
+		for(b=0;b<=1;b++)
+		{
+			for(c=0;c<=1;c++)
+			{
+				printf("%d\t%d\t%d\t%d\n",a,b,c,a|(b|c));
+			}
+		}
+	}
+}
